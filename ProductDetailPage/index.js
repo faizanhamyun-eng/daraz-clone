a635809cbaf2bd7f24362e9474c5e882d50fce9f
faizanhamyun-eng <faizanhamyun@gmail.com>
2025-08-28 17:47:39 +0500

    counter fix

---
+++ ProductDetailPage/index.js
@@ -0,0 +1,22 @@
+console.log("JS loaded ✅");
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".quantity-box").forEach(box => {
+    const minusBtn = box.querySelector(".quantity-minus");
+    const plusBtn = box.querySelector(".quantity-plus");
+    const input = box.querySelector(".quantity-input");
+
+    minusBtn.addEventListener("click", () => {
+      let value = parseInt(input.value) || 1;
+      if (value > 1) {
+        input.value = value - 1;
+      }
+    });
+
+    plusBtn.addEventListener("click", () => {
+      let value = parseInt(input.value) || 1;
+      input.value = value + 1;
+    });
+  });
+});
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
+console.log("JS loaded ✅");
+
 const loadMoreBtn = document.getElementById("loadMoreBtn");
 const loader = document.getElementById("loader");
 const moreProducts = document.getElementById("moreProducts");
 
 loadMoreBtn.addEventListener("click", () => {
-  // Button hide sirf loader ke waqt
   loadMoreBtn.style.display = "none";
   loader.style.display = "flex";
 
   setTimeout(() => {
     loader.style.display = "none";
     moreProducts.classList.add("show"); 
-
-    // Button wapas visible ho (permanently)
     loadMoreBtn.style.display = "inline-block";
   }, 2000);
 });
+
